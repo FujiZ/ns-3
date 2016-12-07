9d7728d6e7a857f50640f11f582ed4bb40b94662
FujiZ <me@zhouhp.com>
2016-12-07 15:08:04 +0800

    try to implement tbf

--- src/dcn/model/token-bucket-filter.cc
+++ src/dcn/model/token-bucket-filter.cc
@@ -1,6 +1,127 @@
+#include "ns3/log.h"
+#include "ns3/assert.h"
+#include "ns3/uinteger.h"
+#include "ns3/drop-tail-queue.h"
+
 #include "token-bucket-filter.h"
 
-TokenBucketFilter::TokenBucketFilter()
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("TokenBucketFilter");
+
+NS_OBJECT_ENSURE_REGISTERED (TokenBucketFilter);
+
+TypeId
+TokenBucketFilter::GetTypeId (void)
 {
+  static TypeId tid = TypeId ("ns3::TokenBucketFilter")
+      .SetParent<Object> ()
+      .SetGroupName ("DCN")
+      .AddAttribute ("DataRate",
+                     "The default data rate for TBF in bps",
+                     DataRateValue (DataRate ("32768b/s")),
+                     MakeDataRateAccessor (&TokenBucketFilter::m_rate),
+                     MakeDataRateChecker ())
+      .AddAttribute ("Bucket",
+                     "The default bucket for TBF",
+                     UintegerValue (32768),
+                     MakeUintegerAccessor (&TokenBucketFilter::m_bucket),
+                     MakeUintegerChecker<uint64_t> ())
+  ;
+  return tid;
+}
+
+TokenBucketFilter::TokenBucketFilter ()
+{
+  NS_LOG_FUNCTION (this);
+  m_tokens = bucket;
+  m_lastUpdateTime = Simulator::Now ();
+  m_queue = GetObject<DropTailQueue> ();
+}
+
+TokenBucketFilter::~TokenBucketFilter ()
+{
+  m_timer.Cancel ();
+}
+
+void
+TokenBucketFilter::SetSendTarget (SendTargetCallback cb)
+{
+  NS_LOG_FUNCTION (this);
+  m_sendTarget = cb;
+}
+
+TokenBucketFilter::SendTargetCallback
+TokenBucketFilter::GetSendTarget ()
+{
+  return m_sendTarget;
+}
+
+void
+TokenBucketFilter::Receive (Ptr<Packet> p)
+{
+  NS_LOG_FUNCTION (this << p);
+
+  //enque packets appropriately if a non-zero queue already exists
+  if (!m_queue->IsEmpty ())
+    {
+      // leave the drop decision to queue
+      m_queue->Enqueue (p);
+    }
+  else
+    {
+      UpdateTokens ();
+      uint64_t packetSize =  (uint64_t)p->GetSize ()<<3; //packet size in bits
+      if (m_tokens >= packetSize)
+        {
+          m_sendTarget (p);
+          m_tokens -= packetSize;
+        }
+      else
+        {
+          bool result = m_queue->Enqueue (p);
+          if (result)
+            {
+              ScheduleSend (p);
+            }
+        }
+    }
+}
+
+void
+TokenBucketFilter::GetUpdatedTokens ()
+{
+  double now = Simulator.Now ().GetSeconds ();
+  m_tokens = std::min (m_bucket,
+                       m_tokens+(now-m_lastUpdateTime)*m_rate.GetBitRate ());
+  m_lastUpdateTime = Simulator.Now ();
+}
+
+void
+TokenBucketFilter::Timeout ()
+{
+  NS_ASSERT (!m_queue->IsEmpty ());
+  Ptr<Packet> p = m_queue->Dequeue ()->GetPacket ();
+  UpdateTokens ();
+  uint64_t packetSize =  (uint64_t)p->GetSize ()<<3; //packet size in bits
+
+  //We simply send the packet here without checking if we have enough tokens
+  //since the timer is supposed to fire at the right time
+  m_sendTarget (p);
+  m_tokens -= packetSize;
+  if(!m_queue->IsEmpty ())
+    {
+      //schedule next event
+      ScheduleSend (m_queue->Peek ()->GetPacket ());
+    }
+}
+
+void
+TokenBucketFilter::ScheduleSend (Ptr<Packet> p)
+{
+  uint64_t packetSize = (uint64_t)p->GetSize ()<<3;
+  m_timer.Schedule (Time::FromDouble ((packetSize-m_tokens)/m_rate.GetBitRate (), Time::S);
+}
 
 }
+
--- src/dcn/model/token-bucket-filter.h
+++ src/dcn/model/token-bucket-filter.h
@@ -1,14 +1,87 @@
 #ifndef TOKEN_BUCKET_FILTER_H
 #define TOKEN_BUCKET_FILTER_H
 
-#include "ns3/object.h"
+#include <stdint.h>
 
-namespace ns3
-{
+#include "ns3/object.h"
+#include "ns3/ptr.h"
+#include "ns3/type-id.h"
+#include "ns3/data-rate.h"
+#include "ns3/nstime.h"
+#include "ns3/timer.h"
+
+namespace ns3 {
+
+class Queue;
+class Packet;
+
+/**
+ * \ingroup dcn
+ *
+ * \brief implementation of TBF in ns-2
+ * yet another implementation of TBF(Token Bucket filter)
+ */
 class TokenBucketFilter : public Object
 {
 public:
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId (void);
+
   TokenBucketFilter ();
+  virtual ~TokenBucketFilter ();
+
+  /**
+   * \brief callback to send packets
+   */
+  typedef Callback<void,Ptr<Packet> > SendTargetCallback;
+
+  /**
+   * This method allows a caller to set the current send target callback
+   *
+   * \param cb current Callback for send target
+   */
+  void SetSendTarget (SendTargetCallback cb);
+
+  /**
+   * This method allows a caller to get the current send target callback
+   *
+   * \return current Callback for send target
+   */
+  void GetSendTarget (void);
+
+  /**
+   * \brief This function is called by sender end when sending packets
+   * \param p the packet for filter to receive
+   */
+  void Receive(Ptr<Packet> p);
+
+private:
+  /**
+   * \brief Update the tokens in TBF
+   */
+  void UpdateTokens (void);
+
+  /**
+   * @brief Schedule the event that send the specific packet
+   * @param p the packet tobe send
+   */
+  void ScheduleSend (Ptr<Packet> p);
+
+  /**
+   * @brief Timeout function called by timer
+   */
+  void Timeout (void);
+
+  SendTargetCallback m_sendTarget;
+  DataRate m_rate;
+  uint64_t m_bucket;
+  double m_tokens;
+  Time m_lastUpdateTime;
+  Ptr<Queue> m_queue;
+  Timer m_timer;
 };
 
 }
