92fef6a26de99815279deb57872fdc49403cacbd
FujiZ <me@zhouhp.com>
2017-04-24 09:49:11 +0800

    update dctcp test

--- scratch/dctcp-test.cc
+++ scratch/dctcp-test.cc
@@ -1,15 +1,3 @@
-/** Network topology
- *
- *    10Mb/s, 2ms
- * n0--------------|
- *                 |   1.5Mbps/s, 20ms
- *                 n2------------------n3
- *    10Mb/s, 3ms  |
- * n1--------------|
- *
- *
- */
-
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
@@ -18,12 +6,23 @@
 #include "ns3/applications-module.h"
 #include "ns3/traffic-control-module.h"
 
+#include <sstream>
+#include <map>
+#include <fstream>
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("DctcpTest");
 
 uint32_t checkTimes;
 double avgQueueSize;
+
+// attributes
+std::string link_data_rate;
+std::string link_delay;
+uint32_t packet_size;
+uint32_t queue_size;
+uint32_t threhold;
 
 // The times
 double global_start_time;
@@ -32,14 +31,25 @@ double sink_start_time;
 double sink_stop_time;
 double client_start_time;
 double client_stop_time;
+double client_interval_time;
 
-NodeContainer n0n2;
-NodeContainer n1n2;
-NodeContainer n2n3;
+// nodes
+NodeContainer nodes;
+NodeContainer clients;
+NodeContainer switchs;
+NodeContainer servers;
 
-Ipv4InterfaceContainer i0i2;
-Ipv4InterfaceContainer i1i2;
-Ipv4InterfaceContainer i2i3;
+// server interfaces
+Ipv4InterfaceContainer serverInterfaces;
+
+// receive status
+std::map<uint32_t, uint64_t> totalRx;   //fId->receivedBytes
+std::map<uint32_t, uint64_t> lastRx;
+
+// throughput result
+std::map<uint32_t, std::vector<std::pair<double, double> > > throughputResult; //fId -> list<time, throughput>
+
+QueueDiscContainer queueDiscs;
 
 std::stringstream filePlotQueue;
 std::stringstream filePlotQueueAvg;
@@ -65,15 +75,144 @@ CheckQueueSize (Ptr<QueueDisc> queue)
 }
 
 void
+TxTrace (uint32_t flowId, Ptr<const Packet> p)
+{
+  NS_LOG_FUNCTION (flowId << p);
+  FlowIdTag flowIdTag;
+  flowIdTag.SetFlowId (flowId);
+  p->AddByteTag (flowIdTag);
+}
+
+void
+RxTrace (Ptr<const Packet> packet, const Address &from)
+{
+  NS_LOG_FUNCTION (packet << from);
+  FlowIdTag flowIdTag;
+  bool retval = packet->FindFirstMatchingByteTag (flowIdTag);
+  NS_ASSERT (retval);
+  if (totalRx.find (flowIdTag.GetFlowId ()) != totalRx.end ())
+    {
+      totalRx[flowIdTag.GetFlowId ()] += packet->GetSize ();
+    }
+  else
+    {
+      totalRx[flowIdTag.GetFlowId ()] = packet->GetSize ();
+      lastRx[flowIdTag.GetFlowId ()] = 0;
+    }
+}
+
+void
+CalculateThroughput (void)
+{
+  for (auto it = totalRx.begin (); it != totalRx.end (); ++it)
+    {
+      double cur = (it->second - lastRx[it->first]) * (double) 8/1e5; /* Convert Application RX Packets to MBits. */
+      throughputResult[it->first].push_back (std::pair<double, double> (Simulator::Now ().GetSeconds (), cur));
+      lastRx[it->first] = it->second;
+    }
+  Simulator::Schedule (MilliSeconds (100), &CalculateThroughput);
+}
+
+void
+SetName (void)
+{
+  // add name to clients
+  int i = 0;
+  for(auto it = clients.Begin (); it != clients.End (); ++it)
+    {
+      std::stringstream ss;
+      ss << "CL" << i++;
+      Names::Add (ss.str (), *it);
+    }
+  i = 0;
+  for(auto it = switchs.Begin (); it != switchs.End (); ++it)
+    {
+      std::stringstream ss;
+      ss << "SW" << i++;
+      Names::Add (ss.str (), *it);
+    }
+  i = 0;
+  for(auto it = servers.Begin (); it != servers.End (); ++it)
+    {
+      std::stringstream ss;
+      ss << "SE" << i++;
+      Names::Add (ss.str (), *it);
+    }
+}
+
+void
+BuildTopo (uint32_t clientNo, uint32_t serverNo)
+{
+  NS_LOG_INFO ("Create nodes");
+  clients.Create (clientNo);
+  switchs.Create (2);
+  servers.Create (serverNo);
+
+  SetName ();
+
+  NS_LOG_INFO ("Install internet stack on all nodes.");
+  InternetStackHelper internet;
+  internet.Install (clients);
+  internet.Install (switchs);
+  internet.Install (servers);
+
+
+  TrafficControlHelper tchPfifo;
+  // use default limit for pfifo (1000)
+  uint16_t handle = tchPfifo.SetRootQueueDisc ("ns3::PfifoFastQueueDisc", "Limit", UintegerValue (queue_size));
+  tchPfifo.AddInternalQueues (handle, 3, "ns3::DropTailQueue", "MaxPackets", UintegerValue (queue_size));
+
+  TrafficControlHelper tchRed;
+  tchRed.SetRootQueueDisc ("ns3::RedQueueDisc", "LinkBandwidth", StringValue (link_data_rate),
+                           "LinkDelay", StringValue (link_delay));
+
+  NS_LOG_INFO ("Create channels");
+  PointToPointHelper p2p;
+
+  p2p.SetQueue ("ns3::DropTailQueue");
+  p2p.SetDeviceAttribute ("DataRate", StringValue (link_data_rate));
+  p2p.SetChannelAttribute ("Delay", StringValue (link_delay));
+
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+  for (auto it = clients.Begin (); it != clients.End (); ++it)
+    {
+      NetDeviceContainer devs = p2p.Install (NodeContainer (*it, switchs.Get (0)));
+      tchPfifo.Install (devs);
+      ipv4.Assign (devs);
+      ipv4.NewNetwork ();
+    }
+  for (auto it = servers.Begin (); it != servers.End (); ++it)
+    {
+      NetDeviceContainer devs = p2p.Install (NodeContainer (switchs.Get (1), *it));
+      tchPfifo.Install (devs);
+      serverInterfaces.Add (ipv4.Assign (devs).Get (1));
+      ipv4.NewNetwork ();
+    }
+  {
+    p2p.SetQueue ("ns3::DropTailQueue");
+    p2p.SetDeviceAttribute ("DataRate", StringValue (link_data_rate));
+    p2p.SetChannelAttribute ("Delay", StringValue (link_delay));
+    NetDeviceContainer devs = p2p.Install (switchs);
+    // only backbone link has RED queue disc
+    queueDiscs = tchRed.Install (devs);
+    ipv4.Assign (devs);
+  }
+  // Set up the routing
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+}
+
+void
 BuildAppsTest (void)
 {
   // SINK is in the right side
   uint16_t port = 50000;
   Address sinkLocalAddress (InetSocketAddress (Ipv4Address::GetAny (), port));
   PacketSinkHelper sinkHelper ("ns3::TcpSocketFactory", sinkLocalAddress);
-  ApplicationContainer sinkApp = sinkHelper.Install (n2n3.Get (1));
+  ApplicationContainer sinkApp = sinkHelper.Install (servers.Get (0));
   sinkApp.Start (Seconds (sink_start_time));
   sinkApp.Stop (Seconds (sink_stop_time));
+  sinkApp.Get (0)->TraceConnectWithoutContext ("Rx", MakeCallback (&RxTrace));
 
   // Connection one
   // Clients are in left side
@@ -81,178 +220,114 @@ BuildAppsTest (void)
    * Create the OnOff applications to send TCP to the server
    * onoffhelper is a client that send data to TCP destination
    */
-  OnOffHelper clientHelper1 ("ns3::TcpSocketFactory", Address ());
-  clientHelper1.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
-  clientHelper1.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
-  clientHelper1.SetAttribute ("DataRate", DataRateValue (DataRate ("10Mb/s")));
-  clientHelper1.SetAttribute ("PacketSize", UintegerValue (1000));
 
-  ApplicationContainer clientApps1;
-  AddressValue remoteAddress (InetSocketAddress (i2i3.GetAddress (1), port));
-  clientHelper1.SetAttribute ("Remote", remoteAddress);
-  clientApps1.Add (clientHelper1.Install (n0n2.Get (0)));
-  clientApps1.Start (Seconds (client_start_time));
-  clientApps1.Stop (Seconds (client_stop_time));
+  OnOffHelper clientHelper ("ns3::TcpSocketFactory", InetSocketAddress (serverInterfaces.GetAddress (0), port));
+  clientHelper.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
+  clientHelper.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
+  clientHelper.SetAttribute ("DataRate", DataRateValue (DataRate (link_data_rate)));
+  clientHelper.SetAttribute ("PacketSize", UintegerValue (packet_size));
 
-  // Connection two
-  OnOffHelper clientHelper2 ("ns3::TcpSocketFactory", Address ());
-  clientHelper2.SetAttribute ("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1]"));
-  clientHelper2.SetAttribute ("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0]"));
-  clientHelper2.SetAttribute ("DataRate", DataRateValue (DataRate ("10Mb/s")));
-  clientHelper2.SetAttribute ("PacketSize", UintegerValue (1000));
+  ApplicationContainer clientApps = clientHelper.Install (clients);
 
-  ApplicationContainer clientApps2;
-  clientHelper2.SetAttribute ("Remote", remoteAddress);
-  clientApps2.Add (clientHelper2.Install (n1n2.Get (0)));
-  clientApps2.Start (Seconds (3.0));
-  clientApps2.Stop (Seconds (client_stop_time));
+  // set different start/stop time for each app
+  double clientStartTime = client_start_time;
+  double clientStopTime = client_stop_time;
+  uint32_t i = 1;
+  for (auto it = clientApps.Begin (); it != clientApps.End (); ++it)
+    {
+      Ptr<Application> app = *it;
+      app->SetStartTime (Seconds (clientStartTime));
+      app->SetStopTime (Seconds (clientStopTime));
+      app->TraceConnectWithoutContext ("Tx", MakeBoundCallback (&TxTrace, i++));
+      clientStartTime += client_interval_time;
+      clientStopTime -=  client_interval_time;
+    }
 }
 
 void
-ConfigLog (void)
+SetConfig (bool useEcn, bool useDctcp)
 {
-  // LogComponentEnable ("RedQueueDisc", LOG_LEVEL_INFO);
-  // LogComponentEnable ("DctcpSocket", LOG_LEVEL_INFO);
-  // LogComponentEnable ("TcpSocketBase", LOG_LEVEL_DEBUG);
-}
 
-void
-ConfigTcp (void)
-{
-  // 42 = headers size
-  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (1000 - 42));
+  GlobalValue::Bind ("ChecksumEnabled", BooleanValue (false));
+  // RED params
+  NS_LOG_INFO ("Set RED params");
+  Config::SetDefault ("ns3::RedQueueDisc::Mode", StringValue ("QUEUE_MODE_PACKETS"));
+  Config::SetDefault ("ns3::RedQueueDisc::MeanPktSize", UintegerValue (packet_size));
+  // Config::SetDefault ("ns3::RedQueueDisc::Gentle", BooleanValue (false));
+  Config::SetDefault ("ns3::RedQueueDisc::QW", DoubleValue (1.0));
+  Config::SetDefault ("ns3::RedQueueDisc::UseMarkP", BooleanValue (true));
+  Config::SetDefault ("ns3::RedQueueDisc::MarkP", DoubleValue (2.0));
+  Config::SetDefault ("ns3::RedQueueDisc::MinTh", DoubleValue (threhold));
+  Config::SetDefault ("ns3::RedQueueDisc::MaxTh", DoubleValue (threhold));
+  Config::SetDefault ("ns3::RedQueueDisc::QueueLimit", UintegerValue (queue_size));
+
+  // TCP params
+  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (packet_size));
   Config::SetDefault ("ns3::TcpL4Protocol::SocketType", StringValue ("ns3::TcpNewReno"));
   // Config::SetDefault ("ns3::TcpSocket::DelAckCount", UintegerValue (1));
-  // Dctcp params
-  Config::SetDefault ("ns3::TcpL4Protocol::SocketBaseType", TypeIdValue(TypeId::LookupByName ("ns3::DctcpSocket")));
-  Config::SetDefault ("ns3::dcn::DctcpSocket::DctcpWeight", DoubleValue (1.0 / 16));
+  if (useEcn)
+    {
+      Config::SetDefault ("ns3::TcpSocketBase::UseEcn", BooleanValue (true));
+      Config::SetDefault ("ns3::RedQueueDisc::UseEcn", BooleanValue (true));
+      if (useDctcp)
+        {
+          Config::SetDefault ("ns3::TcpL4Protocol::SocketBaseType", TypeIdValue(TypeId::LookupByName ("ns3::DctcpSocket")));
+          Config::SetDefault ("ns3::DctcpSocket::DctcpWeight", DoubleValue (1.0 / 16));
+        }
+    }
 }
 
 int
 main (int argc, char *argv[])
 {
-  ConfigLog ();
 
-  std::string redLinkDataRate = "19Mbps";
-  std::string redLinkDelay = "10ms";
-
-  bool useEcn = true;
+  // LogComponentEnable ("DctcpSocket", LOG_LEVEL_DEBUG);
+  bool useEcn = false;
+  bool useDctcp = false;
   std::string pathOut;
   bool writeForPlot = false;
   bool writePcap = false;
   bool flowMonitor = false;
+  bool writeThroughput = false;
 
   bool printRedStats = true;
 
   global_start_time = 0.0;
-  global_stop_time = 11;
+  global_stop_time = 10.0;
   sink_start_time = global_start_time;
   sink_stop_time = global_stop_time + 3.0;
   client_start_time = sink_start_time + 0.2;
-  client_stop_time = global_stop_time - 2.0;
+  client_stop_time = global_stop_time;
+  client_interval_time = 2.0;
+
+  link_data_rate = "1000Mbps";
+  link_delay = "50us";
+  packet_size = 1024;
+  queue_size = 250;
+  threhold = 20;
 
   // Will only save in the directory if enable opts below
   pathOut = "."; // Current directory
   CommandLine cmd;
   cmd.AddValue ("useEcn", "<0/1> to use ecn in test", useEcn);
+  cmd.AddValue ("useDctcp", "<0/1> to use dctcp in test", useDctcp);
   cmd.AddValue ("pathOut", "Path to save results from --writeForPlot/--writePcap/--writeFlowMonitor", pathOut);
   cmd.AddValue ("writeForPlot", "<0/1> to write results for plot (gnuplot)", writeForPlot);
   cmd.AddValue ("writePcap", "<0/1> to write results in pcapfile", writePcap);
   cmd.AddValue ("writeFlowMonitor", "<0/1> to enable Flow Monitor and write their results", flowMonitor);
+  cmd.AddValue ("writeThroughput", "<0/1> to write throughtput results", writeThroughput);
 
   cmd.Parse (argc, argv);
 
-  NS_LOG_INFO ("Create nodes");
-  NodeContainer c;
-  c.Create (4);
-  Names::Add ( "N0", c.Get (0));
-  Names::Add ( "N1", c.Get (1));
-  Names::Add ( "N2", c.Get (2));
-  Names::Add ( "N3", c.Get (3));
-  n0n2 = NodeContainer (c.Get (0), c.Get (2));
-  n1n2 = NodeContainer (c.Get (1), c.Get (2));
-  n2n3 = NodeContainer (c.Get (2), c.Get (3));
-
-  GlobalValue::Bind ("ChecksumEnabled", BooleanValue (false));
-
-
-  uint32_t meanPktSize = 500;
-
-  // RED params
-  NS_LOG_INFO ("Set RED params");
-  Config::SetDefault ("ns3::RedQueueDisc::Mode", StringValue ("QUEUE_MODE_PACKETS"));
-  Config::SetDefault ("ns3::RedQueueDisc::MeanPktSize", UintegerValue (meanPktSize));
-  Config::SetDefault ("ns3::RedQueueDisc::Wait", BooleanValue (true));
-  Config::SetDefault ("ns3::RedQueueDisc::Gentle", BooleanValue (true));
-  Config::SetDefault ("ns3::RedQueueDisc::QW", DoubleValue (0.002));
-  Config::SetDefault ("ns3::RedQueueDisc::MinTh", DoubleValue (20));
-  Config::SetDefault ("ns3::RedQueueDisc::MaxTh", DoubleValue (20));
-  Config::SetDefault ("ns3::RedQueueDisc::QueueLimit", UintegerValue (250));
-
-  ConfigTcp ();
-
-  if (useEcn) // test like 1, but with ecn
-    {
-      Config::SetDefault ("ns3::TcpSocketBase::UseEcn", BooleanValue (true));
-      Config::SetDefault ("ns3::RedQueueDisc::UseEcn", BooleanValue (true));
-    }
-
-  NS_LOG_INFO ("Install internet stack on all nodes.");
-  InternetStackHelper internet;
-  internet.Install (c);
-
-  TrafficControlHelper tchPfifo;
-  uint16_t handle = tchPfifo.SetRootQueueDisc ("ns3::PfifoFastQueueDisc");
-  tchPfifo.AddInternalQueues (handle, 3, "ns3::DropTailQueue", "MaxPackets", UintegerValue (1000));
-
-  TrafficControlHelper tchRed;
-  tchRed.SetRootQueueDisc ("ns3::RedQueueDisc", "LinkBandwidth", StringValue (redLinkDataRate),
-                           "LinkDelay", StringValue (redLinkDelay));
-
-  NS_LOG_INFO ("Create channels");
-  PointToPointHelper p2p;
-
-  p2p.SetQueue ("ns3::DropTailQueue");
-  p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
-  p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
-  NetDeviceContainer devn0n2 = p2p.Install (n0n2);
-  tchPfifo.Install (devn0n2);
-
-  p2p.SetQueue ("ns3::DropTailQueue");
-  p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
-  p2p.SetChannelAttribute ("Delay", StringValue ("3ms"));
-  NetDeviceContainer devn1n2 = p2p.Install (n1n2);
-  tchPfifo.Install (devn1n2);
-
-  p2p.SetQueue ("ns3::DropTailQueue");
-  p2p.SetDeviceAttribute ("DataRate", StringValue (redLinkDataRate));
-  p2p.SetChannelAttribute ("Delay", StringValue (redLinkDelay));
-  NetDeviceContainer devn2n3 = p2p.Install (n2n3);
-  // only backbone link has RED queue disc
-  QueueDiscContainer queueDiscs = tchRed.Install (devn2n3);
-
-  NS_LOG_INFO ("Assign IP Addresses");
-  Ipv4AddressHelper ipv4;
-
-  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
-  i0i2 = ipv4.Assign (devn0n2);
-
-  ipv4.SetBase ("10.1.2.0", "255.255.255.0");
-  i1i2 = ipv4.Assign (devn1n2);
-
-  ipv4.SetBase ("10.1.3.0", "255.255.255.0");
-  i2i3 = ipv4.Assign (devn2n3);
-
-  // Set up the routing
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
+  SetConfig (useEcn, useDctcp);
+  BuildTopo (3, 1);
   BuildAppsTest ();
 
   if (writePcap)
     {
       PointToPointHelper ptp;
       std::stringstream stmp;
-      stmp << pathOut << "/red";
+      stmp << pathOut << "/dctcp";
       ptp.EnablePcapAll (stmp.str ());
     }
 
@@ -272,6 +347,11 @@ main (int argc, char *argv[])
       remove (filePlotQueueAvg.str ().c_str ());
       Ptr<QueueDisc> queue = queueDiscs.Get (0);
       Simulator::ScheduleNow (&CheckQueueSize, queue);
+    }
+
+  if (writeThroughput)
+    {
+      Simulator::Schedule (Seconds (sink_start_time), &CalculateThroughput);
     }
 
   Simulator::Stop (Seconds (sink_stop_time));
@@ -302,6 +382,19 @@ main (int argc, char *argv[])
       std::cout << "\t " << st.qLimDrop << " drops due queue full" << std::endl;
     }
 
+  if (writeThroughput)
+    {
+      for (auto& resultList : throughputResult)
+        {
+          std::stringstream ss;
+          ss << pathOut << "/throughput-" << resultList.first << ".txt";
+          std::ofstream out (ss.str ());
+          for (auto& entry : resultList.second)
+            {
+              out << entry.first << "," << entry.second << std::endl;
+            }
+        }
+    }
   Simulator::Destroy ();
 
   return 0;
